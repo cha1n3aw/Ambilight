641c058a0cd7693760e636bee20f364f85191110
THE DEAD <fpv555@gmail.com>
2020-03-30 15:42:05 +0500

    added prevention of 2 instances of program at once

--- Ambilight_PC_Software/Program.cs
+++ Ambilight_PC_Software/Program.cs
@@ -1,16 +1,28 @@
 ﻿using System;
+using System.Runtime.InteropServices;
+using System.Threading;
 using System.Windows.Forms;
 
-namespace WindowsFormsApp4
+namespace WinForms_Dynamic_Ambilight
 {
     static class Program
     {
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new DynamicAmbilight.DynamicAmbilight());
+            GuidAttribute attribute = (GuidAttribute)typeof(Program).Assembly.GetCustomAttributes(typeof(GuidAttribute), true)[0];
+            using (Mutex mutex = new Mutex(false, @"Global\" + attribute.Value))
+            {
+                if (!mutex.WaitOne(0, false))
+                {
+                    MessageBox.Show("An instance of Dynamic Ambilight is already running");
+                    return;
+                }
+                GC.Collect();
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new DynamicAmbilight.DynamicAmbilight());
+            } 
         }
     }
 }
